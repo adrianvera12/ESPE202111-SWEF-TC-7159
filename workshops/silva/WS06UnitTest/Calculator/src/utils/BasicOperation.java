39c7c6961e2bc86bb1d5ece2d4705aa811dad52b
Raul Silva <raul29247@gmail.com>
2022-02-17 18:53:12 -0500

    silva0.1.1

---
+++ workshops/silva/WS06UnitTest/Calculator/src/utils/BasicOperation.java
@@ -0,0 +1,20 @@
+
+package utils;
+
+/**
+ *
+ * @author raul2
+ */
+public class BasicOperation {
+    
+    public float add(float adden1, float adden2){
+        float total;
+        total = adden1+adden2;
+        return total;
+    }
+    public float subtruct(float minuend, float sustrahend){
+        float difference;
+        difference = minuend+sustrahend;
+        return difference;
+    }
+}
