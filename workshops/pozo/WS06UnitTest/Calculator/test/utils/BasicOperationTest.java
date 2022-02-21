66c2c101da40354ba137d0e80957efa62c29b014
JAlexis11 <jpozomontaluisa@gmail.com>
2022-02-20 22:22:31 -0500

    Calculator v.1.1

--- workshops/pozo/WS06UnitTest/Calculator/test/utils/BasicOperationTest.java
+++ workshops/pozo/WS06UnitTest/Calculator/test/utils/BasicOperationTest.java
@@ -52,7 +52,62 @@ public class BasicOperationTest {
         BasicOperation instance = new BasicOperation();
         float expResult = -1.4F;
         float result = instance.subtract(minuend, subtrahend);
-        assertEquals(expResult, result, 0.0);
+        assertEquals(expResult, result, 0.001);
     }
-    
+
+    /**
+     * Test of multiplication method, of class BasicOperation.
+     */
+    @Test
+    public void testMultiplication() {
+        System.out.println("multiplication");
+        float multiplier = 1.2F;
+        float multiplicando = 1.3F;
+        BasicOperation instance = new BasicOperation();
+        float expResult = 1.56F;
+        float result = instance.multiplication(multiplier, multiplicando);
+        assertEquals(expResult, result, 0.001);
+        // TODO review the generated test code and remove the default call to fail.
+    }
+     public void testMultiplicationCero() {
+        System.out.println("multiplication");
+        float multiplier = 0.0F;
+        float multiplicand = 0.0F;
+        BasicOperation instance = new BasicOperation();
+        float expResult = 0.0F;
+        float result = instance.multiplication(multiplier, multiplicand);
+        assertEquals(expResult, result, 0.001);
+        // TODO review the generated test code and remove the default call to fail.
+    }
+     public void testMultiplicationNegative() {
+        System.out.println("multiplication");
+        float multiplier = -1.0F;
+        float multiplicand = -2.0F;
+        BasicOperation instance = new BasicOperation();
+        float expResult = 2.0F;
+        float result = instance.multiplication(multiplier, multiplicand);
+        assertEquals(expResult, result, 0.001);
+    }
+    /**
+     * Test of division method, of class BasicOperation.
+     */
+    @Test
+    public void testDivision() {
+        System.out.println("division");
+        float dividend = 6.0F;
+        float divisor = 3.0F;
+        BasicOperation instance = new BasicOperation();
+        float expResult = 2.0F;
+        float result = instance.division(dividend, divisor);
+        assertEquals(expResult, result, 0.0001);
+    }
+    public void testDivisionCero() {
+        System.out.println("division");
+        float dividend = 0.0F;
+        float divisor = 1.0F;
+        BasicOperation instance = new BasicOperation();
+        float expResult = 0.0F;
+        float result = instance.division(dividend, divisor);
+        assertEquals(expResult, result, 0.001);
+    }
 }
