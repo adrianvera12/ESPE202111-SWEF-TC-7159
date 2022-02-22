d84e36fd60b7c8ed540b8d34fc5142fcc8214c91
afvillacres <ppistolas95@gmail.com>
2022-02-21 22:33:27 -0500

    Calculator V1.0

--- workshops/villacres/WS06UnitTest/Calculator/src/utils/BasicOperation.java
+++ workshops/villacres/WS06UnitTest/Calculator/src/utils/BasicOperation.java
@@ -1,9 +1,5 @@
 package utils;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
 /**
  *
