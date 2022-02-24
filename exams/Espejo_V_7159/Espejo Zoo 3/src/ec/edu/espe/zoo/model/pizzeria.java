ccc4673bfb52c4f9a7b08faccb95b7bd7b16c56f
VictorEspejo <victordaniel001@hotmail.com>
2022-02-24 12:44:19 -0500

    exam

---
+++ exams/Espejo_V_7159/Espejo Zoo 3/src/ec/edu/espe/zoo/model/pizzeria.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.zoo.model;
+
+/**
+ *
+ * @author danielespejo
+ */
+public class pizzeria {
+    int Id;
+    String Name;
+    String phoneNumber;
+    String address;
+    String email;
+    SocialNetwork [] socialNetworks;
+            
+    
+    
+}
